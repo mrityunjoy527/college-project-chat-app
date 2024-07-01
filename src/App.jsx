47f33b3ca424771d252e5618f101d9545332d636
Mrityunjoy Barman <mrityunjoy527@gmail.com>
2024-07-01 13:09:48 +0530

    Toggle Login Register

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import "./app.css";
 import ChatContainer from "./components/chatContainer/ChatContainer";
 import Register from "./components/register/Register.jsx";
 import Login from "./components/login/Login.jsx";
+import useRegister from "./useRegister.js";
 
 function App() {
+
+  const { wantToRegister } = useRegister();
+  const user = null;
 
   return (
     <div className="outerContainer">
       <div className="bg"></div>
-      <ChatContainer />
-      {/* <Register/> */}
-      {/* <Login/> */}
+      {
+        user ? <ChatContainer /> : wantToRegister ? <Register /> : <Login />
+      }
+
     </div>
   );
 }
